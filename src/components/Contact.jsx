669fcfe3654d95ea4f2374e8aaf102f7d92b9fb2
Tomsss <tomasito21010@gmail.com>
2024-02-02 21:05:15 -0300

    Primer commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const miCorreoElectronico = 'tomassanchez2101@gmail.com';
 const Contact = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gray-300 text-[#2c2c2c]'>
+    <div name='contact' className='w-full h-screen bg-gray-300 text-[#2c2c2c]'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,6 +48,11 @@ const Navbar = () => {
             Work
           </Link>
         </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
         
       </ul>
 
@@ -85,6 +90,12 @@ const Navbar = () => {
           {' '}
           <Link onClick={handleClick} to='work' smooth={true} duration={500}>
             Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
           </Link>
         </li>
         
