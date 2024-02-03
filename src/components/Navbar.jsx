57a5652684259f7faa49ff3e0287b1f28d13c83b
Tomsss <tomasito21010@gmail.com>
2024-02-03 00:36:58 -0300

    Primer commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,7 +115,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2c2c2c]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://https://github.com/Sanhats'
+              href='https://github.com/Sanhats'
             >
               Github <FaGithub size={30} />
             </a>
