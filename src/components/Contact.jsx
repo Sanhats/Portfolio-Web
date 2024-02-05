49ade2d7c0806f4d8a16ef03add6f5897da990e2
Tomsss <103080220+Sanhats@users.noreply.github.com>
2024-02-05 13:26:09 -0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,6 +22,12 @@ const Contact = () => {
             <a href={`mailto:${miCorreoElectronico}`}  className=' border-2 border-gray-800 bg-transparent py-2.5 px-5 font-medium uppercase text-gray-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-gray-800 before:transition-transform before:duration-300 before:content-[] hover:text-white before:hover:scale-x-100'>
                 Email
             </a>
+               <a
+              className='border-2 border-gray-800 bg-transparent py-2.5 px-5 font-medium uppercase text-gray-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-gray-800 before:transition-transform before:duration-300 before:content-[] hover:text-white before:hover:scale-x-100'
+              href='https://funny-pixie-075.notion.site/TOMAS-EMANUEL-SANCHEZ-RESUME-2817caa582ed474984bc48c5e66e770a'
+            >
+              Resume 
+            </a>
             </ul>
           </div>
       </div>
