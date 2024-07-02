6291ca8b4d47fb2a1453514c1b6f56cd86de9eec
Tomsss <103080220+Sanhats@users.noreply.github.com>
2024-07-02 13:43:03 -0300

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,6 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
